66798fd71a1d5dadd8cdfe395a0961865f8290d4
kritikasingh66 <kritikasrinet66@gmail.com>
2026-07-24 12:37:57 +0530

    Refactor Firebase config and imports

--- firebase-config.js
+++ firebase-config.js
@@ -1,55 +1,20 @@
-// firebase-config.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
-import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-
-// Your Firebase configuration
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-
-    apiKey: "AIzaSyDVqW0IuoquzCY5Nx42oNybe_0XibpoR4Y",
-
-    authDomain: "attendance-system-8796f.firebaseapp.com",
-
-    projectId: "attendance-system-8796f",
-
-    storageBucket: "attendance-system-8796f.firebasestorage.app",
-
-    messagingSenderId: "652144461336",
-
-    appId: "1:652144461336:web:79aa4049ed1a6d8ea3e56d",
-
-    measurementId: "G-9M1CFTEK3K"
-
+  apiKey: "AIzaSyDVqW0IuoquzCY5Nx42oNybe_0XibpoR4Y",
+  authDomain: "attendance-system-8796f.firebaseapp.com",
+  projectId: "attendance-system-8796f",
+  storageBucket: "attendance-system-8796f.firebasestorage.app",
+  messagingSenderId: "652144461336",
+  appId: "1:652144461336:web:79aa4049ed1a6d8ea3e56d",
+  measurementId: "G-9M1CFTEK3K"
 };
-
-
-// Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 
-
-// Firebase Authentication
-
 const auth = getAuth(app);
-
-
-// Firestore Database
-
 const db = getFirestore(app);
 
-
-// Export Firebase services
-
-export {
-    auth,
-    db
-};
+export { auth, db };
